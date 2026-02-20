ca0d01a0d67a31cb2fd90158ecd1e24dff696d59
Max Kohler <hello@maxkohler.com>
2026-02-20 18:51:03 +0100

    Add HTML preview

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,13 @@
 import typescript from "@rollup/plugin-typescript"
 import terser from "@rollup/plugin-terser"
-import copy from "@guanghechen/rollup-plugin-copy"
-import serve from "rollup-plugin-serve"
 
 export default {
 	input: {
 		main: "src/index.ts"
 	},
 	output: {
-		dir: "dist",
+		dir: "_site",
 		entryFileNames: "[name].js"
 	},
-	plugins: [
-		copy({
-			watch: "views",
-			targets: [{ src: "views/*.html", dest: "dist" }]
-		}),
-		typescript(),
-		terser(),
-		serve("dist")
-	]
+	plugins: [typescript(), terser()]
 }
